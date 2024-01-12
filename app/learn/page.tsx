1e9e64af9b4532b1d1037faaaf1fb68ddb1c9fa8
iwgithub2 <86887820+iwgithub2@users.noreply.github.com>
2024-01-11 20:42:50 -0500

    added a map

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,20 +1,29 @@
+'use client'
 import Link from "next/link";
 import {newsreader} from "@/app/ui/fonts";
 import Search from "@/app/ui/search";
 import State from "@/app/ui/learn/state";
 import Map from "@/app/ui/learn/map";
+import {ComposableMap, Geographies, Geography} from "react-simple-maps";
 
 export default function Page() {
-    return(
+    // @ts-ignore
+    return (
         <main className="flex min-h-screen flex-col p-6">
             <div className={"flex flex-col justify-center mt-40"}>
                 <p className={`${newsreader.className} text-6xl md-text-xl text-center py-10 tracking-tighter`}>
                     Tell us where you are so we can<br/> find the information for you.
                 </p>
                 <Search placeholder={'State...'}/>
-                <div className="relative bg-gray-100  mx-40 mt-40 aspect-square">
-                    <Map/>
-                </div>
+                <ComposableMap width={10000} height={10000} projection={"geoAlbers"}>
+                    <Geographies geography="/states.json">
+                        {({geographies}) =>
+                            geographies.map((geo) => (
+                                <Geography key={geo.rsmKey} geography={geo}/>
+                            ))
+                        }
+                    </Geographies>
+                </ComposableMap>
             </div>
         </main>
     );
